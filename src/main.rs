539a1588ae15ffb9fee8c95f7e8c7a0f9ea1f40f
gabrielQ12 <gabriel.quinette1@gmail.com>
2024-01-12 13:02:50 +0100

    opérateur arithmétiques

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,35 @@
-fn main() {
-    let mut foo =10;
-    println!("La valeur de foo est : {foo}.");
-    foo = 20;
-    println!("La valeur de foo est : {foo}.");
+// fn main() {
+//     let mut foo =10;
+//     println!("La valeur de foo est : {foo}.");
+//     foo = 20;
+//     println!("La valeur de foo est : {foo}.");
 
-    const FOO_BAR: i32 = 10;
-    println!("La valeur de FOO_BAR est : {FOO_BAR}.");
+//     const FOO_BAR: i32 = 10;
+//     println!("La valeur de FOO_BAR est : {FOO_BAR}.");
 
-    let bar = 10;
-    let bar = bar +1;
+//     let bar = 10;
+//     let bar = bar +1;
 
-    {
-        let bar = bar-6; //5
-        println!("La valeur du bar a l'interieur acolade est : {bar}.");
+//     {
+//         let bar = bar-6; //5
+//         println!("La valeur du bar a l'interieur acolade est : {bar}.");
 
-    }
-     //11
-    println!("La valeur de bar est : {bar}.");
+//     }
+//      //11
+//     println!("La valeur de bar est : {bar}.");
     
+// }
+
+fn main() {
+    // println!("{}, {}", "Hello", "world!");
+    let x =10;
+    let y = 5;
+
+    println!("{}",x + y ); //montre seulement le resultat
+    println!("{} + {} = {}",x, y, x + y ); //montre toute l'opération
+    println!("{} - {} = {}",x, y, x - y );
+    println!("{} * {} = {}",x, y, x * y );
+    println!("{} / {} = {}",x, y, x / y );
+    println!("{} % {} = {}",x, y, x % y );
+
 }
