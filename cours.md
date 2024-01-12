c6d29de59b0b0f14cedeae1e66d6379d680d0829
gabrielQ12 <gabriel.quinette1@gmail.com>
2024-01-12 17:02:59 +0100

    arrays

--- cours.md
+++ cours.md
@@ -185,6 +185,14 @@ fn main() {
 
     ```Rust
 
+fn main() {
+    let mut x =0;
+
+    for x in 1..11 {
+        if x == 5 { break; }
+        println!("{}", x);
+    }
+} 
   ```
 
     ```Rust
--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,23 @@
 
 fn main() {
-    let mut x =0;
+   let mut x = ["This", "world", "displays", "hello"];
 
-    for x in 1..11 {
-        if x == 5 { break; }
-        println!("{}", x);
-    }
+   println!("{} {} {} {}.", x[0], x[2], x[3], x[1]);
+   println!("La taille de l'array est : {}.", x.len()); //len ==> length
+
+   x[1] = "Alex";
+   println!("{} {} {} {}.", x[0], x[2], x[3], x[1]);
+
+   let mut y = ["Foo", "Alex"];
+   y[1] = "bar";
+   println!("{} {}.", y[0], y[1]);
+
+   let mut z = [3; 15];
+   println!("La taille de l'array est : {}.", z.len()); //len ==> length
+   z[10] = 50;
+   println!("{} {}.", z[5], z[10]);
+
+   for i in 0..z.len(){
+   println!("index {}: {} " , i, z[i]);
 } 
+}
