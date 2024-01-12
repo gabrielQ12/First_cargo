c98550c0fba2c4605678a3992b359b19c6ad3f29
gabrielQ12 <gabriel.quinette1@gmail.com>
2024-01-12 12:41:14 +0100

    variable et mutabilité

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
 fn main() {
-    println!("Hello, world!");
+    let mut foo =10;
+    println!("La valeur de foo est : {foo}.");
+    foo = 20;
+    println!("La valeur de foo est : {foo}.");
+
+    const FOO_BAR: i32 = 10;
+    println!("La valeur de FOO_BAR est : {FOO_BAR}.");
+
+    let bar = 10;
+    let bar = bar +1;
+
+    {
+        let bar = bar-6; //5
+        println!("La valeur du bar a l'interieur acolade est : {bar}.");
+
+    }
+     //11
+    println!("La valeur de bar est : {bar}.");
+    
 }
