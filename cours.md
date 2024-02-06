49a468d3685cba97874765d4754b610c8a174cf6
gabrielQ12 <gabriel.quinette1@gmail.com>
2024-02-06 14:35:14 +0100

    methode

--- cours.md
+++ cours.md
@@ -507,7 +507,31 @@ fn foo() -> String {
 -------------
 
 ```Rust
+#[derive(Debug)]
 
+struct Triangle {
+    base: u32,
+    side: u32,
+}
+
+fn main() {
+    // Triangle isocèle : base + (2 * coté)
+    // base : 8cm, coté : 10 cm -> 8+(2*10) = 28 cm
+    // x = base , y = coté
+    let triangle = Triangle {
+        base: 8,
+        side: 10,
+    };
+
+    println! (
+        "Notre triangle : {:?} \nLe périmètre d'un triangle isocèle de {}cm et de  {}cm est égal à :{}cm",
+        triangle, triangle.base, triangle.side, calculate_perimetre(&triangle)
+    );
+}
+
+fn calculate_perimetre(triangle: &Triangle) -> u32 {
+    triangle.base + (2* triangle.side)
+}
 ```
 
 -------------
--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,58 @@
-#[derive(Debug)]
+
 
 struct Triangle {
     base: u32,
     side: u32,
 }
 
+impl Triangle {
+    fn calculate_perimetre(&self) -> u32 {
+        self.base + (2* self.side)
+    }
+
+    fn is_base_greater_then_side(&self) -> bool {
+        self.base > self.side
+    }
+
+    fn is_triangle_bigger(&self, other: &Triangle) -> bool {
+        self.calculate_perimetre() > other.calculate_perimetre()
+    }
+}
+
 fn main() {
-    // Triangle isocèle : base + (2 * coté)
+    // first_triangle isocèle : base + (2 * coté)
     // base : 8cm, coté : 10 cm -> 8+(2*10) = 28 cm
     // x = base , y = coté
-    let triangle = Triangle {
-        base: 8,
-        side: 10,
+    let first_triangle = Triangle {
+        base: 15,
+        side: 12
     };
 
+    let second_triangle = Triangle {
+        base:22,
+        side:25
+    };
+
+
+
     println! (
-        "Notre triangle : {:?} \nLe périmètre d'un triangle isocèle de {}cm et de  {}cm est égal à :{}cm",
-        triangle, triangle.base, triangle.side, calculate_perimetre(&triangle)
+        "Le périmètre d'un first_triangle isocèle de {}cm et de  {}cm est égal à :{}cm",
+        first_triangle.base, first_triangle.side, first_triangle.calculate_perimetre()
+    );
+
+    println!(
+        "La base est-elle plus grande que le côté ? : {}",
+        first_triangle.is_base_greater_then_side()
+    );
+
+    println!(
+        "La base est-elle plus grande que le côté ? : {}",
+        second_triangle.is_base_greater_then_side()
+    );
+
+    println!(
+        " Le premier triangle est il plus grand que le second ? :{}  ({} > {})",
+        first_triangle.is_triangle_bigger(&second_triangle), first_triangle.calculate_perimetre(), second_triangle.calculate_perimetre()
     );
 }
 
-fn calculate_perimetre(triangle: &Triangle) -> u32 {
-    triangle.base + (2* triangle.side)
-}
