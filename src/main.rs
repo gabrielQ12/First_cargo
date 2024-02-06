cd8757b7fca3cbfe5f2a36c17952bfa8a89fc892
gabrielQ12 <gabriel.quinette1@gmail.com>
2024-02-06 14:04:23 +0100

    triangle

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,25 @@
+#[derive(Debug)]
 
-
-fn main() {
-    let bar = foo();
-    println!("{bar}");
+struct Triangle {
+    base: u32,
+    side: u32,
 }
 
-fn foo() -> String {
-    let s = String::from("Alex");
-    s
+fn main() {
+    // Triangle isocèle : base + (2 * coté)
+    // base : 8cm, coté : 10 cm -> 8+(2*10) = 28 cm
+    // x = base , y = coté
+    let triangle = Triangle {
+        base: 8,
+        side: 10,
+    };
+
+    println! (
+        "Notre triangle : {:?} \nLe périmètre d'un triangle isocèle de {}cm et de  {}cm est égal à :{}cm",
+        triangle, triangle.base, triangle.side, calculate_perimetre(&triangle)
+    );
+}
+
+fn calculate_perimetre(triangle: &Triangle) -> u32 {
+    triangle.base + (2* triangle.side)
 }
